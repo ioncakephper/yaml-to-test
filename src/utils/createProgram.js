f36b36d6c86e76908c707c648c00a47ce8819b55
Ion Gireada <shytiger@yahoo.com>
2025-07-18 09:58:34 +0000

    Ioncakephper/issue1

--- src/utils/createProgram.js
+++ src/utils/createProgram.js
@@ -1,5 +1,12 @@
 const { Command } = require("commander");
 
+/**
+ * This function attempts to read the package.json file and returns its contents.
+ * If the file cannot be read, it logs an error and returns an empty object.
+ * @returns {Object} - Object with package.json data
+ * @description Loads package.json data to provide metadata for the CLI program.
+ * @throws {Error} - Throws an error if package.json cannot be loaded.
+ */
 function loadPackage() {
   try {
     const packageData = require("../../package.json");
